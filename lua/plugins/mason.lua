bedbc72ec37845f0278a2f85a72d89ebe7e69389
suoliw <_hebo@hrbeu.edu.cn>
2025-09-29 17:29:34 +0800

    配置lsp

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,44 @@
+return {
+    "mason-org/mason.nvim",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "mason-org/mason-lspconfig.nvim",
+    },
+    event = "VeryLazy",
+    opts={},
+
+    config = function(_, opts)
+        require("mason").setup(opts)
+        local registry = require("mason-registry")
+
+        local function setup(name, config)
+            local success, package = pcall(registry.get_package, name)
+            if success and not package:is_installed() then
+                package:install()
+            end
+
+            local nvim_lsp = require("mason-lspconfig").get_mappings().package_to_lspconfig[name]
+            vim.lsp.config(nvim_lsp, config)
+
+        end
+
+        LuaSettings = {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {"vim"}
+                    }
+                }
+            }
+        }
+
+        setup("lua-language-server", LuaSettings)
+
+        vim.cmd("LspStart")
+        vim.diagnostic.config({
+            virtual_text = true,
+            undate_in_insert = true,
+        })
+    end
+
+}
